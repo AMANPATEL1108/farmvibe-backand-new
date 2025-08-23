ec893f79332538da9c4141b4257efadc2d181ef9
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-23 17:42:46 +0530

    Done

---
+++ src/main/java/com/example/farmvibe_backand_new/api/FarmvibeBackandNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmvibe_backand_new.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmvibeBackandNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmvibeBackandNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/farmvibe_backand_new/api/FarmvibeBackandNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmvibe_backand_new.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmvibeBackandNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
