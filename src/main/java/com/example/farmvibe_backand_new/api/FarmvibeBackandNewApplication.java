ee6e32e36b77204b73d9b100692ff6858976093f
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-11-08 16:57:50 +0530

    Remove Details from Applciation properties also in this code Comepledt to added a Model for product Details also in this Profile page in that Compelted Worked and edit udpaet delete for Adderss and user details Compelted

--- src/main/java/com/example/farmvibe_backand_new/api/FarmvibeBackandNewApplication.java
+++ src/main/java/com/example/farmvibe_backand_new/api/FarmvibeBackandNewApplication.java
@@ -3,7 +3,6 @@ package com.example.farmvibe_backand_new.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FarmvibeBackandNewApplication {
 
