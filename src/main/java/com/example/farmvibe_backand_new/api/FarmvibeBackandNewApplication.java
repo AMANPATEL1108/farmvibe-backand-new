d2b2c0812d58b60474c7ca88e1742b38a5d8b15b
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-11-08 16:56:40 +0530

    Done

--- src/main/java/com/example/farmvibe_backand_new/api/FarmvibeBackandNewApplication.java
+++ src/main/java/com/example/farmvibe_backand_new/api/FarmvibeBackandNewApplication.java
@@ -3,6 +3,7 @@ package com.example.farmvibe_backand_new.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FarmvibeBackandNewApplication {
 
